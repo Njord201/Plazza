93cddbd7b2f35d10ad324929daaf2b04509e9b6a
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-05-14 16:28:19 +0200

    feat: add the Pizza class

---
+++ src/Pizza.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** Plazza
+** File description:
+** Pizza
+*/
+
+#include "Pizza.hpp"
+
+Plazza::Pizza::Pizza(Plazza::PizzaType type, Plazza::PizzaSize size)
+    : _type(type), _size(size)
+{
+}
