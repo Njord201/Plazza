7b8e0a7963c1ea0d5af90d365ab6d1e58c887cc6
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-05-14 17:02:26 +0200

    feat: add parsing of input in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,23 @@
 ** main
 */
 
+#include "Input.hpp"
+
+#include <iostream>
+
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    Plazza::InputParser parser = Plazza::InputParser();
+    while (1) {
+        try {
+            parser.parseLine();
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    (void) ac;
+    (void) av;
 
     return 0;
 }
